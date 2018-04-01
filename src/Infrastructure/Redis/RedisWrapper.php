2dcf2504cc23f8b5594d51a47786661aa58c10e9
Holger Woltersdorf <hw@hollo.me>
2018-04-01 14:00:06 +0200

    Add redis wrapper to lazy connect to redis server

---
+++ src/Infrastructure/Redis/RedisWrapper.php
@@ -0,0 +1,63 @@
+<?php declare(strict_types=1);
+
+namespace hollodotme\Readis\Infrastructure\Redis;
+
+use hollodotme\Readis\Infrastructure\Interfaces\ProvidesConnectionData;
+use hollodotme\Readis\Infrastructure\Redis\Exceptions\ConnectionFailedException;
+use Redis;
+
+final class RedisWrapper
+{
+	/** @var ProvidesConnectionData */
+	private $connectionData;
+
+	/** @var Redis */
+	private $redis;
+
+	/** @var bool */
+	private $connected;
+
+	public function __construct( ProvidesConnectionData $connectionData )
+	{
+		$this->connectionData = $connectionData;
+		$this->redis          = new Redis();
+	}
+
+	/**
+	 * @param string $name
+	 * @param array  $arguments
+	 *
+	 * @return mixed
+	 * @throws ConnectionFailedException
+	 */
+	public function __call( string $name, array $arguments )
+	{
+		$this->connectToServer();
+
+		return $this->redis->{$name}( ...$arguments );
+	}
+
+	/**
+	 * @throws ConnectionFailedException
+	 */
+	private function connectToServer() : void
+	{
+		if ( $this->connected )
+		{
+			return;
+		}
+
+		$this->connected = @$this->redis->connect(
+			$this->connectionData->getHost(),
+			$this->connectionData->getPort(),
+			$this->connectionData->getTimeout(),
+			'',
+			$this->connectionData->getRetryInterval()
+		);
+
+		if ( !$this->connected )
+		{
+			throw (new ConnectionFailedException())->withConnectionData( $this->connectionData );
+		}
+	}
+}
