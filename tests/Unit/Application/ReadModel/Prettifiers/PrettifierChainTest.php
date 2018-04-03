a172e2686fa7a9481edc99867848fb9848b33936
Holger Woltersdorf <hw@hollo.me>
2018-04-03 12:18:24 +0200

    Add prettifier chain tests

---
+++ tests/Unit/Application/ReadModel/Prettifiers/PrettifierChainTest.php
@@ -0,0 +1,79 @@
+<?php declare(strict_types=1);
+
+namespace hollodotme\Readis\Tests\Unit\Application\ReadModel\Prettifiers;
+
+use hollodotme\Readis\Application\ReadModel\Interfaces\PrettifiesString;
+use hollodotme\Readis\Application\ReadModel\Prettifiers\PrettifierChain;
+use PHPUnit\Framework\ExpectationFailedException;
+use PHPUnit\Framework\TestCase;
+use SebastianBergmann\RecursionContext\InvalidArgumentException;
+
+final class PrettifierChainTest extends TestCase
+{
+	/**
+	 * @param string $input
+	 * @param string $expectedOutput
+	 *
+	 * @throws ExpectationFailedException
+	 * @throws InvalidArgumentException
+	 *
+	 * @dataProvider inputProvider
+	 */
+	public function testCanPrettifyStrings( string $input, string $expectedOutput ) : void
+	{
+		$chain = new PrettifierChain();
+		$chain->addPrettifiers(
+			$this->getPrettifierMock( 'A' ),
+			$this->getPrettifierMock( 'B' )
+		);
+
+		$this->assertTrue( $chain->canPrettify( $input ) );
+		$this->assertSame( $expectedOutput, $chain->prettify( $input ) );
+	}
+
+	public function inputProvider() : array
+	{
+		return [
+			[
+				'input'          => 'SAM',
+				'expectedOutput' => 'A: SAM',
+			],
+			[
+				'input'          => 'BEN',
+				'expectedOutput' => 'B: BEN',
+			],
+			[
+				'input'          => 'BENJAMIN',
+				'expectedOutput' => 'A: BENJAMIN',
+			],
+			[
+				'input'          => 'CRIS',
+				'expectedOutput' => 'CRIS',
+			],
+		];
+	}
+
+	private function getPrettifierMock( string $needle ) : PrettifiesString
+	{
+		return new class($needle) implements PrettifiesString
+		{
+			/** @var string */
+			private $needle;
+
+			public function __construct( string $needle )
+			{
+				$this->needle = $needle;
+			}
+
+			public function canPrettify( string $data ) : bool
+			{
+				return false !== strpos( $data, $this->needle );
+			}
+
+			public function prettify( string $data ) : string
+			{
+				return $this->needle . ': ' . $data;
+			}
+		};
+	}
+}
