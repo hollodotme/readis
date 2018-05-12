f9907fb0f5adaefc573200d5515f49ca34c47ee7
Holger Woltersdorf <hw@hollo.me>
2018-05-13 00:28:56 +0200

    Add sorted set sub key data builder

---
+++ src/Application/ReadModel/KeyDataBuilders/SortedSetSubKeyDataBuilder.php
@@ -0,0 +1,59 @@
+<?php declare(strict_types=1);
+
+namespace hollodotme\Readis\Application\ReadModel\KeyDataBuilders;
+
+use hollodotme\Readis\Application\Interfaces\ProvidesKeyInfo;
+use hollodotme\Readis\Application\ReadModel\Constants\KeyType;
+use hollodotme\Readis\Application\ReadModel\DTO\KeyData;
+use hollodotme\Readis\Application\ReadModel\Interfaces\BuildsKeyData;
+use hollodotme\Readis\Application\ReadModel\Interfaces\PrettifiesString;
+use hollodotme\Readis\Application\ReadModel\Interfaces\ProvidesKeyData;
+use hollodotme\Readis\Application\ReadModel\Interfaces\ProvidesKeyName;
+use hollodotme\Readis\Exceptions\RuntimeException;
+use hollodotme\Readis\Infrastructure\Redis\ServerManager;
+
+final class SortedSetSubKeyDataBuilder implements BuildsKeyData
+{
+	/** @var ServerManager */
+	private $manager;
+
+	/** @var PrettifiesString */
+	private $prettifier;
+
+	public function __construct( ServerManager $manager, PrettifiesString $prettifier )
+	{
+		$this->manager    = $manager;
+		$this->prettifier = $prettifier;
+	}
+
+	public function canBuildKeyData( ProvidesKeyInfo $keyInfo, ProvidesKeyName $keyName ) : bool
+	{
+		return $keyName->hasSubKey() && (KeyType::SORTED_SET === $keyInfo->getType());
+	}
+
+	/**
+	 * @param ProvidesKeyInfo $keyInfo
+	 * @param ProvidesKeyName $keyName
+	 *
+	 * @throws RuntimeException
+	 * @return ProvidesKeyData
+	 */
+	public function buildKeyData( ProvidesKeyInfo $keyInfo, ProvidesKeyName $keyName ) : ProvidesKeyData
+	{
+		$i = 0;
+		foreach ( $keyInfo->getSubItems() as $member => $score )
+		{
+			if ( (int)$keyName->getSubKey() !== $i++ )
+			{
+				continue;
+			}
+
+			$rawKeyData = $member;
+			$keyData    = $this->prettifier->prettify( $member );
+
+			return new KeyData( $keyData, $rawKeyData, $score );
+		}
+
+		throw new RuntimeException( 'Could not find key in sorted set anymore.' );
+	}
+}
